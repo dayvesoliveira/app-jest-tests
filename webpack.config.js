f797d0765eecb19a88be181cbf175bb3cc564a2c
Dayves Oliveira <dayvesoliveira@gmail.com>
2019-09-27 11:39:58 -0300

    remocao de todo  cra da criacao do app

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,36 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
-    output: {
-        path: path.resolve('dist'),
-        filename: '[name].[hash].js',
-        publicPath: '/'
+    output:{
+        path: path.resolve(__dirname, '/dist'),
+        filename: 'bundle.js'
     },
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
-          },
-          {
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: ['css-loader', 'sass-loader']
-            })
-          }
+    module:{
+        rules:[
+            {
+                test: /\.js|(x)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3000
-    },
-    plugins: [
-        new ExtractTextPlugin('css/styles.css')
-    ]
-      
-};
+        compress: false,
+        port: 3000,
+        overlay: {
+            warnings: true,
+            errors: true
+        }
+    }
+}
--- webpack.prod.config.js
+++
@@ -1,4 +0,0 @@
-//- mock: criar uma store de teste e um provider de teste
-// jest-cucumber (BDD) --> yarn add --dev jest-cucumber
-// Chai
-
