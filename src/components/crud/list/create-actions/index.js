0ae95df2a8b81a43e7e4c11bf09a4190448418b0
Dayves Oliveira <dayvesoliveira@gmail.com>
2019-10-02 18:09:37 -0300

    implementando o redux

--- src/components/crud/list/create-actions/index.js
+++ src/components/crud/list/create-actions/index.js
@@ -1,3 +1,9 @@
+import api from '../../../../config/api'
+
 // definir aqui as actions
+
+import api from '../../../../config/api'
+
+
 
 export default { }
---
+++ src/config/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import Auth from './auth'
+
+export const envirioments = {
+    END_POINT: "https://reqres.in/",
+
+}
+
+const api = axios.create({
+    baseURL: envirioments.END_POINT
+})
+
+api.interceptors.request.use(async config => {
+    const token = Auth.getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
+export default api
