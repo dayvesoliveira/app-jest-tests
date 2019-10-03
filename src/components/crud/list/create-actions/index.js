b26be0ef7b85c2ac560b6ff8e59f729041182073
Dayves Oliveira <dayvesoliveira@gmail.com>
2019-10-03 17:50:15 -0300

    implementacao de testes

--- src/components/crud/list/create-actions/index.js
+++ src/components/crud/list/create-actions/index.js
@@ -1,10 +1,42 @@
 import api from '../../../../config/api'
 
 // definir aqui as actions
+const POSTS_GET_ALL         = "POSTS_GET_ALL"
+const POSTS_GET_ALL_SUCCESS = "POSTS_GET_ALL_SUCCESS"
+const POSTS_GET_ALL_ERROR   = "POSTS_GET_ALL_ERROR"
 
-import api from '../../../../config/api'
+export const PostsTypeActions = {
+    POSTS_GET_ALL,
+    POSTS_GET_ALL_SUCCESS,
+    POSTS_GET_ALL_ERROR
+}
+
+const END_POINT = '/posts'
 
 
+const getAllPosts = () => ({type: POSTS_GET_ALL })
+
+const setAllPosts = payload => dispatch => ({
+    type: POSTS_GET_ALL_SUCCESS,
+    payload
+})
+
+const errorAllPosts = () => ({type: POSTS_GET_ALL_ERROR })
+
+const fetchAllPosts = () => async dispatch =>{
+    dispatch(getAllPosts())
+    try {
+        const response = await api.get(END_POINT)
+
+        setAllPosts(response)
+
+    } catch(e){
+        dispatch(errorAllPosts())
+    }
+}
 
 
-export default { }
+export default { 
+    fetchAllPosts,
+    setAllPosts
+}
--- src/components/crud/list/reducer/index.js
+++ src/components/crud/list/reducer/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux"
+import { PostsTypeActions as caPosts } from "../create-actions"
 
 const initialState = {
     list: [],
@@ -8,6 +9,27 @@ const initialState = {
 }
 
 // definir aqui os reducers
+
+const posts = ( state = initialState, {type, payload} ) => {
+    switch (type){
+        case caPosts.POSTS_SET_ALL:
+            return payload
+
+        case caPosts.POSTS_GET_ALL:
+            return {
+                ...state,
+                list: [],
+                isFetching: true,
+                complete: false,
+                status: 0
+            }
+        case caPosts.POSTS_GET_ALL_SUCCESS:
+        return {
+
+        }
+        case caPosts.POSTS_GET_ALL_ERROR:
+    }
+}
 
 const reducer = combineReducers({  })
 
