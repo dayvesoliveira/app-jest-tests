0db8250908139d7d796a0e1b5f1d39155397db37
Dayves Oliveira <dayvesoliveira@gmail.com>
2019-10-30 08:30:46 -0300

    implementacao do detail somente visualizacao

--- src/components/crud/detail/presentation/detail/index.js
+++ src/components/crud/detail/presentation/detail/index.js
@@ -2,11 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Label = styled.label`
+    font: normal 10px/24px arial, helvetica;
     display: block;
-    margin: 15px auto;
+    margin: 15px 10px;
     padding: 15px;
     font-size: 1.2em;
     min-width: 50%;
+    background-color: #e0e0e0;
+    border-radius: 10px;
+`
+const Span = styled.span`
+    display: flex;
+    margin: 5px 0;
+    padding: 10px 5px;
+    font-size: .7em;
+    background-color: coral;
+    width: 34px;
+    border-radius: 100%;
+    justify-content: center;
+    color: #fff;
 `
 
 const userByid = (users, userId) => {
@@ -24,9 +38,9 @@ const DetailComponent = ({
     listUsers
 }) =>(
     <>
-        <Label>Title: { title }</Label>
-        <Label>Body:  { body }</Label>
-        <Label>User:  { userByid(listUsers, userId) }</Label>
+        <Label><Span>Title</Span> { title }</Label>
+        <Label><Span>Body</Span>  { body }</Label>
+        <Label><Span>User</Span>  { userByid(listUsers, userId) }</Label>
     </>
 )
 
